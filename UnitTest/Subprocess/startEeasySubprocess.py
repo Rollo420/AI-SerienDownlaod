ac9344d5d8efbb393ff8a0bf663006bb6ce4d0af
Rollo420 <peterhannes550@gmail.com>
2025-08-08 15:13:49 +0200

    Updated code in UnitTest/GetEpisode and app/downloadManager.py, removed docker-compose.yml service video-downloader-app, and modified docker/PiHole/adlists.list

--- UnitTest/Subprocess/startEeasySubprocess.py
+++ UnitTest/Subprocess/startEeasySubprocess.py
@@ -1,6 +1,11 @@
 import asyncio
 import json
 import random
+import subprocess
+import sys
+
+semaphore = asyncio.Semaphore(4)  # Limit concurrent tasks to 4
+
 
 def load_json_data(filename):
     print("Loading JSON data...")
@@ -26,27 +31,47 @@ async def get_series_data(serien):
                     "episode_links": episode_links
                 }
 
-async def print_series_data(serien):
-    print(json.dumps(serien, indent=4))
-    await asyncio.sleep(random.uniform(1, 10))  # Simulate processing time
+async def create_task(agent_name, data):
+    print(
+        f"Creating task for {data['title']} Season {data['season_number']} Episode {data['episode_links']['episode_number']} with {agent_name}..."
+    )
+    return await asyncio.create_subprocess_exec(
+            sys.executable,
+            "./app/downloader/VOE.py",
+            agent_name,
+            data["episode_links"]["primary_link"],
+            f"/app/serien/{data['title']}/Season-{data['season_number']}/",
+        
+    )
+async def start_task(agent_name, task):
+    print(f"{agent_name}: Waiting for semaphore...")  # Klarere Ausgabe
+    async with semaphore:
+        print(f"{agent_name}: Acquired semaphore, starting task...")
+        await asyncio.create_task(task)
+        print(f"{agent_name}: Task completed.")
     
-semaphore = asyncio.Semaphore(3)  # Limit concurrent tasks to 3
+    return f"Task completed by {agent_name}."
+
+
 async def main():
     filename = "./UnitTest/Subprocess/all_series_data.json"
     serien = load_json_data(filename)
 
     print("Starting to process series data...\n")
 
+    tasks = []
     process_id = 0
+    print(f"Creating tasks for {len(serien)} series...")
     async for serie in get_series_data(serien):
-        tasks = [print_series_data(episode) for key, episode in serie.items()]
-        print(f"Task: {process_id}, Wait for semaphore to limit concurrent tasks...")
-        async with semaphore:
-            print(f"Task: {process_id}, Semaphore started")
-            process_id += 1
-            await asyncio.gather(*tasks)
-            print(f"Task {process_id}, Semaphore finished")
-
+        process_id += 1
+        agent_name = f"Agent-{process_id}"
+        
+        print(f"[{agent_name}] - Processing {serie['title']} Season {serie['season_number']} Episode {serie['episode_links']['episode_number']}...")
+        tasks.append(await asyncio.create_task(create_task(agent_name, serie)))
+        
+        input(tasks)
+        
+            
 
 if __name__ == "__main__":
     asyncio.run(main())
